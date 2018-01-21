2517c8ea64517fae2dba12cf77f8b152532c1a6c
aquinatus96 <kumiega.tomasz@gmail.com>
2018-01-21 16:43:45 +0100

    key-style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function (e) {
+var playOnKey = function (e) {
 
     var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -10,46 +10,47 @@ window.addEventListener('keydown', function (e) {
     audio.play();
 
     key.classList.add('playing');
+};
 
-});
+window.addEventListener('keydown', playOnKey);
 
 var keys = document.querySelectorAll('.key');
 
 
-[...keys].forEach(key => key.addEventListener('click', playOnClick));
+keys.forEach(key => key.addEventListener('click', playOnClick));
 
 function playOnClick(e) {
 
     var sample = this.getAttribute('data-key');
     var audio = document.querySelector(`audio[data-key="${sample}"]`)
 
-    
+
     if (!sample) {
         return;
     }
-    
+
     audio.currentTime = 0;
     audio.play();
-    
+
     this.classList.add('playing');
 
 }
 
-[...keys].forEach(key => key.addEventListener('touchstart', playOnTouch));
+keys.forEach(key => key.addEventListener('touchstart', playOnTouch));
 
 function playOnTouch(e) {
 
     var sample = this.getAttribute('data-key');
     var audio = document.querySelector(`audio[data-key="${sample}"]`);
 
-    
+
     if (!sample) {
         return;
     }
-    
+
     audio.currentTime = 0;
     audio.play();
-    
+
     this.classList.add('playing');
 
 }
@@ -61,3 +62,61 @@ function removeTransition(e) {
 }
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+/* INPUT MODE SCRIPTS start */
+
+//input mode turn on and off
+
+var btnInputFile = document.querySelector('.file-input');
+
+btnInputFile.addEventListener('click', function () {
+
+    var key = document.getElementsByClassName('key');
+    var keyIcon = document.querySelectorAll('.key .fa');
+    var keyCaption = document.getElementsByClassName('key-caption');
+
+
+
+
+    if (btnInputFile.classList.contains('off')) {
+
+        btnInputFile.classList.remove('off');
+        btnInputFile.classList.add('on');
+
+        //removing functions which playing samples
+        window.removeEventListener('keydown', playOnKey);
+        keys.forEach(key => key.removeEventListener('click', playOnClick));
+        keys.forEach(key => key.removeEventListener('touchstart', playOnTouch));
+        
+        
+                    var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+
+
+        for (var i = 0; i < key.length; i++) {
+
+            key[i].classList.remove('key-style');
+            key[i].classList.add('input-mode');
+            keyCaption[i].classList.add('disable');
+            keyIcon[i].classList.remove('disable');
+        }
+
+    } else if (btnInputFile.classList.contains('on')) {
+
+        btnInputFile.classList.remove('on');
+        btnInputFile.classList.add('off');
+
+        for (var i = 0; i < key.length; i++) {
+
+            key[i].classList.remove('input-mode');
+            key[i].classList.add('key-style');
+            keyCaption[i].classList.remove('disable');
+            keyIcon[i].classList.add('disable');
+        }
+    }
+
+});
+
+
+/* INPUT MODE SCRIPT end */
